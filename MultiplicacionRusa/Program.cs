e0c5fff1ab7f7c29bbe770319fbfd6dff0501916
ewin24 <edwintrigos24@gmail.com>
2023-12-04 05:14:27 -0500

    Agregar archivos de proyecto.

---
+++ MultiplicacionRusa/Program.cs
@@ -0,0 +1,27 @@
+﻿static int MultiplicacionRusa(int multiplicando, int multiplicador)
+{
+    int suma = 0;
+
+    while (multiplicador > 0)
+    {
+        if (multiplicador % 2 != 0)
+        {
+            suma += multiplicando;
+        }
+
+        multiplicando *= 2;
+        multiplicador /= 2;
+    }
+
+    return suma;
+}
+
+
+Console.WriteLine("Ingrese multiplicador: ");
+int multiplicador = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese multiplicando: ");
+int multiplicando = Int32.Parse(Console.ReadLine());
+
+int resultado = MultiplicacionRusa(multiplicando, multiplicador);
+
+Console.WriteLine($"Resultado: {resultado}");
